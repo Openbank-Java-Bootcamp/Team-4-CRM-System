c855a0355bde78686fc0d6f171d15d1020221c2c
YadiCalzadilla <100872227+YadiCalzadilla@users.noreply.github.com>
2022-05-03 18:29:51 +0200

    Menu

--- src/main/java/Classes/Menu.java
+++ src/main/java/Classes/Menu.java
@@ -6,20 +6,48 @@ import java.util.Scanner;
 public class Menu {
 
     public static void welcome(){
-        System.out.println("Welcome to LBL Trucking Company CRM");
-        System.out.println("-----------------------------------");
         System.out.println();
+        System.out.println(Colors.YELLOW_BOLD_BRIGHT + "||       ||ººº\\\\   ||        " + Colors.GREEN_BOLD_BRIGHT + "ººººººººº  ||ºººº\\\\  ||      ||   //ººº\\\\  ||   //  ºº||ºº  ||\\\\    ||   //ººº\\\\");
+        System.out.println(Colors.YELLOW_BOLD_BRIGHT + "||       ||   //   ||           " + Colors.GREEN_BOLD_BRIGHT + "TT      ||    //  ||      ||  //        ||  //     ||    || \\\\   ||  // ");
+        System.out.println(Colors.YELLOW_BOLD_BRIGHT + "||       ||==||    ||           " + Colors.GREEN_BOLD_BRIGHT + "||      ||___//   ||      ||  ||        ||=||      ||    ||  \\\\  ||  ||    ____");
+        System.out.println(Colors.YELLOW_BOLD_BRIGHT + "||____   ||   \\\\   ||____      " + Colors.GREEN_BOLD_BRIGHT + " ||      ||   \\\\    \\\\____//   \\\\        ||  \\\\     ||    ||   \\\\ ||  \\\\     //");
+        System.out.println(Colors.YELLOW_BOLD_BRIGHT + "||____|  ||___//   ||____|      " + Colors.GREEN_BOLD_BRIGHT + "||      ||    \\\\    \\\\__//     \\\\___//  ||   \\\\  __||__  ||    \\\\||   \\\\___//");
+        System.out.println();
+        System.out.println("                                 //ººº\\\\   //ººº\\\\   ||\\\\   //||  ||ºººº\\\\    //\\\\       ||\\\\    ||   \\\\    //");
+        System.out.println("                                //        //     \\\\  || \\\\ // ||  ||    //   //  \\\\      || \\\\   ||    \\\\  //");
+        System.out.println("                                ||        ||     ||  ||  \\=/  ||  ||___//   //____\\\\     ||  \\\\  ||     \\\\//");
+        System.out.println("                                \\\\        \\\\     //  ||       ||  ||       //======\\\\    ||   \\\\ ||      ||");
+        System.out.println("                                 \\\\___//   \\\\___//   ||       ||  ||      //        \\\\   ||    \\\\||      ||");
+
+        System.out.println(Colors.YELLOW_BRIGHT);
+        System.out.println("                                                                                              @codebreakers");
+        System.out.println(Colors.RESET);
+
+        System.out.println();
+        System.out.println("                                       Welcome to LBL Trucking Company CRM. Press ENTER to start the program");
+        startProgram("------------------------------------------------------------------------------------------------------------");
+    }
+
+    public static void startProgram(String start) {
+        System.out.println(start);
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
     }
 
     public static void displayMenu(){
 
-        System.out.println("You have the following options");
+        System.out.print(Colors.GREEN_BOLD_BRIGHT);
+        System.out.println("You have the following options:");
         System.out.println("Please, type the command indicated [in brackets] for the action you want to perform");
-        System.out.println("  - Create a Lead [New Lead]");
-        System.out.println("  - Display list of Leads [Show Leads]");
-        System.out.println("  - Show details of a Lead [Lookup Lead id]");
-        System.out.println("  - Turn a Lead into an Opportunity [convert id]");
-        System.out.println("  - Change Opportunity status [change status]");
+        System.out.println(Colors.RESET);
+        System.out.println("  - Create a Lead" + Colors.YELLOW_BRIGHT + " [New Lead]" + Colors.RESET);
+        System.out.println("  - Display list of Leads"  + Colors.YELLOW_BRIGHT + " [Show Leads]" + Colors.RESET);
+        System.out.println("  - Show details of a Lead"  + Colors.YELLOW_BRIGHT + " [Lookup Lead id]" + Colors.RESET);
+        System.out.println("  - Turn a Lead into an Opportunity" + Colors.YELLOW_BRIGHT + " [convert id]" + Colors.RESET);
+        System.out.println("  - Change Opportunity status"  + Colors.YELLOW_BRIGHT + " [change status]" + Colors.RESET);
+        System.out.println("  - Close the program"  + Colors.YELLOW_BRIGHT + " [exit]" + Colors.RESET);
+        Scanner scanner = new Scanner(System.in);
+        selectOption(scanner);
     }
 
     public static void selectOption(Scanner scanner){
@@ -44,6 +72,11 @@ public class Menu {
                 case "change status":
                     crm.changeOppStatus(scanner);
                     break;
+                case "exit":
+                    System.out.println();
+                    System.out.println(Colors.GREEN_BOLD_BRIGHT + "The program is shutting down");
+                    System.out.println(Colors.YELLOW_BOLD_BRIGHT + "See you soon......");
+                    break;
                 default:
                     System.out.println("Wrong command. Please, try again");
                     option = "";
@@ -51,6 +84,5 @@ public class Menu {
             }
         }
     }
-
 
 }
--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -2,6 +2,7 @@ package Main;
 
 import Classes.CRM;
 import Classes.Lead;
+import Classes.Menu;
 
 import java.util.HashMap;
 import java.util.Scanner;
@@ -9,6 +10,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Menu.welcome();
+        Menu.displayMenu();
+
+
+
+        /*
         Lead pepe = new Lead ("pepe", "398475093", "293847", "029384");
         HashMap<Integer, Lead> leads = new HashMap<>();
         leads.put(pepe.getId(), pepe);
@@ -16,48 +23,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         f.setLeadMap(leads);
         f.convertLead(scanner);
-        /*
-
-        Functionality f = new Functionality();
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Menu:");
-        System.out.println("To create new lead type \"New Lead\"");
-        System.out.println("To see leads id type \"Show Leads\"");
-        System.out.println("To see a lead details type \"Lookup Lead id\"");
-        System.out.println("To covert lead into opportunity type \"convert id\"");
-        System.out.println("To change an opportunity status type \"close-lost id\" or \"close-won id\"");
-
-        String userInput = scanner.nextLine();
-        f.processInput(userInput);
-
-        //metodo que reciba el user inout y analice las distintas posibilidades con sus
-        // requerimientos de formato y excepciones.
-
-
-        //If "New Lead"
-        f.createLead(scanner);
-
-        //If “Show Leads”
-        f.listIdName();
-
-        //If “Lookup Lead "id"”
-        int idmomentaneo = 1; //should read the id from the input
-        f.leadDetail(idmomentaneo);
-
-
-        //If “convert id”
-        f.convertLead(scanner);
-
-
-        //If “close-lost id” or “close-won id” where “id”
-        Status newStatus = Status.CLOSED_LOST; //should read this from the input
-        f.changeOppStatus(idmomentaneo, newStatus);
-
-
-
-
-         */
+        */
 
 
     }
